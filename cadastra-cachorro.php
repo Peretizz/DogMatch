a285ac1553cbe2b950dd1f1602e73c3525db2365
Anna <annacarolinaifc@gmail.com>
2025-10-07 08:58:58 -0300

    dadae

--- cadastra-cachorro.php
+++ cadastra-cachorro.php
@@ -2,7 +2,7 @@
    session_start();
    require_once "src/CachorroDAO.php";
 
-   CachorroDAO::cadastrarCachorro($_POST, $_FILES);
+   CachorroDAO::cadastrarCachorro($_POST);
    $_SESSION['msg'] = "Cachorro cadastrado com sucesso!";
    header("Location: index.php");
 ?>
